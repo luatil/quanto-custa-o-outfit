5a315aba2e9f31488af795a97267f15b790dcb8f
andremuchon <andre.muchon17@gmail.com>
2020-01-19 02:29:40 -0300

    add pants type

--- database/random_wardrobe_generator.py
+++ database/random_wardrobe_generator.py
@@ -2,7 +2,7 @@ import numpy as np
 
 shirt_colortype = ['red', 'blue', 'black', 'white', 'grey']
 formality = ['formal', 'casual']
-pants_type = ['jeans', 'caqui']
+pants_styletype = ['jeans', 'caqui']
 pants_colortype = ['blue', 'black']
 brands = ['renner', 'riachuelo', 'cea']
 
@@ -20,8 +20,9 @@ with open('random_wardrobe.txt', 'w') as f:
         pants_formality = random_element(formality)
         shirt_price = np.random.randint(low = 10, high = 60)
         pants_price = np.random.randint(low = 40, high = 120)
+        pants_type = random_element(pants_styletype)
         shirt_str = f'shirt,{shirt_brand},{shirt_color},{shirt_formality},{shirt_price}\n'
-        pants_str = f'pants,{pants_brand},{pants_color},{pants_formality},{pants_price}\n'
+        pants_str = f'pants,{pants_type},{pants_brand},{pants_color},{pants_formality},{pants_price}\n'
         f.write(shirt_str)
         f.write(pants_str)
 
